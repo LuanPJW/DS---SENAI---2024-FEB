7de972049d7206d1b406655c07790352318e4d3a
LuanSilva <158364340+LuanPJW@users.noreply.github.com>
2024-03-01 16:36:43 -0300

    Create script.js

---
+++ HTML/modal/script.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button");
+const modal = document.querySelector("dialog");
+const buttonclose = document.querySelector("dialog button");
+
+button.onclick = function(){
+
+    modal.show();
+
+}
+buttonclose.onclick = function(){
+
+    modal.close();
+}
